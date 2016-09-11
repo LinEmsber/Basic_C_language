0ced5291ac7454f59c5c58f789d67b8f9c8e7fb8
LinEmsber <LinEmsber@gmail.com>
2016-09-11 23:55:17 +0800

    single_num_01.c add comment

--- LeetCode/single_num_01.c
+++ LeetCode/single_num_01.c
@@ -1,10 +1,26 @@
+/*
+ * Given an array of integers, every element appears twice except for one.
+ * Find that single one.
+ */
+
+#include <stdio.h>
+
 int single_number(int A[], int n)
 {
+	int i;
 	int result = 0;
 
-	for (int i = 0; i<n; i++) {
+	for (i = 0; i<n; i++) {
 		result ^=A[i];
 	}
-	
+
 	return result;
+}
+
+
+int main()
+{
+	int arr[7] = {3, 6, 5, 6, 3, 7, 5};
+
+	printf("single_number: %d\n", single_number(arr, 7) );
 }
