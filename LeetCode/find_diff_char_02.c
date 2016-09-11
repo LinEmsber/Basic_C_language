8d2cc7d4da6d80a910382f522c142990514f66ed
LinEmsber <LinEmsber@gmail.com>
2016-09-11 23:19:29 +0800

    find_diff_char_02.c add XOR comment

--- LeetCode/find_diff_char_02.c
+++ LeetCode/find_diff_char_02.c
@@ -1,5 +1,11 @@
 // 3 ms, XOR method
 
+// int a = 60;				// 60 = 0011 1100
+// int b = 13;				// 13 = 0000 1101
+//
+// a ^ b = 49;				// 49 = 0011 0001
+// a ^ b ^ a = 49 ^ a = 60			// 60 = 0000 1101
+
 char find_diff_char(char* s, char* t)
 {
 	char c = t[0];
