aa09812e8fbdf619b27cd95a59c759a37189b382
LinEmsber <LinEmsber@gmail.com>
2016-09-23 23:14:11 +0800

    intersection of two array

--- LeetCode/intersection_two_arr_01.c
+++ LeetCode/intersection_two_arr_01.c
@@ -8,98 +8,95 @@
 
 
 /* intersection, find the intersection of two integer array
+ *
  * @arr_1: the first array
  * @arr_1_len: the length of array_1
  * @arr_2: the first array
  * @arr_2_len: the length of array_2
- * @
+ * @return_len: the length of return array
  */
-int *intersection(int* arr_1, int arr_1_len, int* arr_2, int arr_2_len, int return_size)
+int *intersection(int* arr_1, int arr_1_len, int* arr_2, int arr_2_len, int *return_len)
 {
 	int i, j;
 	int len = MIN(arr_1_len, arr_2_len);
 
 	int *ret_arr = (int *)malloc( sizeof(int) * len );
-	int ret_arr_index = 0;
+	int ret_arr_index = -1;
 
 	// check each element in arr_1;
 	for( i = 0; i < arr_1_len; i++){
 
-		// arr_2 start from the first element
-		j = 0;
-
 		// check whether the arr_1[i] in array arr_2;
-		for(; j < arr_2_len; j++){
+		for( j = 0; j < arr_2_len; j++){
+
+			// find the same integer
 			if(arr_1[i] == arr_2[j])
-			break;
+				break;
 		}
 
+		// save arr_1[i] into return array, ret_arr
+		// ensure that j less than arr_2_len
 		if ( j < arr_2_len ){
-			ret_arr[ret_arr_index] = arr_2[j];
-			ret_arr_index++;
+
+			// avoid duplicate, compare with return array
+			for(j = 0; j <= ret_arr_index; j++){
+				if(ret_arr[j] == arr_1[i]) {
+					break;
+				}
+			}
+
+			// if j > ret_arr_index represents that arr_1[i] is not exist at return array,
+			// so we save it into retrun array.
+			if ( j > ret_arr_index ){
+				ret_arr[++ret_arr_index] = arr_1[i];
+			}
 		}
 
-		// if(j < arr_2_len){
-		// 	j = 0;
-		//
-		// 	// avoid duplicate;
-		// 	for(; j <= ret_arr_index; j++){
-		// 		if(ret_arr[j] == arr_1[i]) {
-		// 			break;
-		// 		}
-		// 	}
-		//
-		// 	if(j > ret_arr_index){
-		// 		ret_arr_index++;
-		// 		ret_arr[ret_arr_index] = arr_1[i];
-		// 	}
-		// }
 	}
-	// return_size = ret_arr_index+1;
+
+	// return len
+	*return_len = ret_arr_index + 1;
 
 	return ret_arr;
 }
 
-/*
+
+/* print out the array with specific length
  *
- *
+ * @arr: the array we want to print
+ * @arr_len: the specific length of array we want to print
  */
 void print_array(int *arr, int arr_len)
 {
 	int i;
 	for (i = 0; i < arr_len; i++){
-		if ( i == 0){
-			printf("arr: ");
-		}
-
 		printf("%d ", *(arr + i ) );
-
-		if (i == (arr_len - 1) ){
-			printf("\n");
-		}
 	}
+
+	printf("\n");
 }
 
 int main()
 {
 	int i;
-	int a_1[] = {2, 30, 100, 3, 5, 500, 6, 4};
-	int a_2[] = {2, 3, 7 ,30, 4, 3, 500, 5, 100, 3, 6, 4};
+	int ret_len;
+
+	int a_1[] = {2, 30, 1000, 3, 5, 500, 60, 4, 7};
+	int a_2[] = {21, 3, 7 ,30, 4, 3, 500, 5, 100, 3, 6, 4};
 
 	int a_1_len = sizeof(a_1) / sizeof( int );
 	int a_2_len = sizeof(a_2) / sizeof( int );
 
 	int min_len = MIN(a_1_len, a_2_len);
 
+	printf("arr_1: ");
 	print_array(a_1, a_1_len);
 
-	// printf("int a_1[8] = {2, 3, 3, 3, 3, 1, 6, 4}\n");
-	// printf("int a_2[12] = {2, 3, 7 ,3, 4, 3, 5, 5, 1, 3, 6, 4}\n");
-	//
-	// for (i = 0; i < 10 ; i++){
-	//
-	// 	printf("intersection(a_1, a_2): %d\n", *(intersection(a_1, 8, a_2, 12, 10)+i) );
-	// }
-	//
-	// // free();
+	printf("arr_2: ");
+	print_array(a_2, a_2_len);
+
+	printf("arr intersection: ");
+	print_array(intersection(a_1, a_1_len, a_2, a_2_len, &ret_len), min_len );
+
+	return 0;
 }
