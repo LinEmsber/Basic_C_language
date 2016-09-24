d7e4ba301aff058672bf074cf15471a19284e759
LinEmsber <LinEmsber@gmail.com>
2016-09-24 23:54:29 +0800

    single number

--- LeetCode/single_num_01.c
+++ LeetCode/single_num_01.c
@@ -5,13 +5,17 @@
 
 #include <stdio.h>
 
-int single_number(int A[], int n)
+int single_number(int arr[], int n)
 {
 	int i;
 	int result = 0;
 
-	for (i = 0; i<n; i++) {
-		result ^=A[i];
+	for (i = 0; i < n; i++) {
+
+		// result = result ^ arr[i];
+		result ^= arr[i];
+
+		// printf("results: %d\n", result);
 	}
 
 	return result;
@@ -20,7 +24,10 @@ int single_number(int A[], int n)
 
 int main()
 {
-	int arr[7] = {3, 6, 5, 6, 3, 7, 5};
+	int arr_1[7] = {3, 6, 5, 6, 3, 9, 5};
+	int arr_2[3] = {3, 6, 3};
 
-	printf("single_number: %d\n", single_number(arr, 7) );
+	printf("single_number: %d\n", single_number(arr_1, 7) );
+	printf("single_number: %d\n", single_number(arr_2, 3) );
+
 }
