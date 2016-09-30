202a0b11de29c70600f534c8dab502c5c5ee8eff
LinEmsber <LinEmsber@gmail.com>
2016-09-30 20:55:50 +0800

    sum left leaves

--- LeetCode/sum_left_leaves_01.c
+++ LeetCode/sum_left_leaves_01.c
@@ -1,26 +1,42 @@
-int sumOfLeftLeaves(struct TreeNode* root) {
+/* There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24. */
+//
+// Definition for a binary tree node.
+// struct TreeNode
+// {
+// 	int val;
+// 	struct TreeNode *left;
+// 	struct TreeNode *right;
+// }
+//
+//	   3
+//	  / \
+//	 9  20
+//	    / \
+//	   15  7
 
+int sumOfLeftLeaves(struct TreeNode* root)
+{
 	if (root == NULL){
 		return 0;
-	}
+	}else{
+		int sum = 0;
 
-	int sum = 0;
+		// check left side first
+		if (root -> left != NULL){
 
-	// check left side first
-	if (root -> left != NULL){
-
-		// check the exist of the deeper left leaves
-		if( root -> left -> left == NULL && root -> left -> right == NULL){
-			sum += root -> left -> val;
-		}else{
-			sum += sumOfLeftLeaves(root -> left);
+			// check the exist of the deeper left leaves
+			if( root -> left -> left == NULL && root -> left -> right == NULL){
+				sum += root -> left -> val;
+			}else{
+				sum += sumOfLeftLeaves(root -> left);
+			}
 		}
+
+
+		if (root -> right != NULL){
+			sum += sumOfLeftLeaves(root -> right);
+		}
+
+		return sum;
 	}
-
-
-	if (root -> right != NULL){
-		sum += sumOfLeftLeaves(root -> right);
-	}
-
-	return sum;
 }
