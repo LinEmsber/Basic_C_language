76914cc92bae4abf19e9b36ec2b18eaa7bb83f64
LinEmsber <LinEmsber@gmail.com>
2016-10-06 22:56:57 +0800

    merge list

---
+++ LeetCode/merge_two_sorted_list_01.c
@@ -0,0 +1,51 @@
+/*
+ * Merge two sorted linked lists and return it as a new list.
+ * The new list should be made by splicing together the nodes of the first two lists.
+ */
+
+typedef node_t struct node;
+
+struct node
+{
+	int val;
+	struct node * next;
+};
+
+node_t* merge_lists(node_t* l_1, node_t* l_2) {
+
+	if(l_1 == NULL) return l_2;
+	if(l_2 == NULL) return l_1;
+
+	node_t *dummy = (node_t*) malloc (sizeof(node_t));
+	dummy->next = NULL;
+
+	node_t *ptr = dummy;
+
+	while (l_1 != NULL && l_2 != NULL)
+	{
+		if (l_1->val < l_2->val)
+		{
+			ptr->next = l_1;
+			l_1 = l_1->next;
+		}
+		else
+		{
+			ptr->next = l_2;
+			l_2 = l_2->next;
+		}
+		ptr = ptr->next;
+	}
+
+	if (l_1 == NULL)
+	{
+		ptr->next = l_2;
+	}
+	else if (l_2 == NULL)
+	{
+		ptr->next = l_1;
+	}
+
+	ptr = dummy->next;
+	free(dummy);
+	return ptr;
+}
