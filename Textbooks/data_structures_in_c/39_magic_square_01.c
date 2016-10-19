b6c1b22dd3bddd04a720d2540a44a6bfd84eb0a4
LinEmsber <LinEmsber@gmail.com>
2016-10-19 23:05:03 +0800

    Ackermann function

--- Textbooks/data_structures_in_c/39_magic_square_01.c
+++ Textbooks/data_structures_in_c/39_magic_square_01.c
@@ -20,7 +20,7 @@
 //
 // }
 
-
+/* FIXME */
 int main()
 {
 	int i, j;
@@ -29,7 +29,7 @@ int main()
 	int size = 5;
 	int square[MAX_SIZE][MAX_SIZE];
 
-
+	// initialize square
 	for (i = 0; i < size; i++)
 		for (j = 0; j < size; j++)
 			// *(square + (i * size) + j) = 0;
@@ -42,11 +42,14 @@ int main()
 	j = (size - 1) / 2;
 
 	for ( count = 2; count <= size * size; count++){
+
 		// up
 		row = (i - 1 < 0) ? (size - 1) : (i - 1);
 
 		// left
-		col = (j - 1 < 0) ? (size - 1) : (j - 1);
+		// col = (j - 1 < 0) ? (size - 1) : (j - 1);
+		col = (j + 1 > size - 1) ? 0 : (j + 1);
+
 
 		// down
 		if (square[row][col]){
