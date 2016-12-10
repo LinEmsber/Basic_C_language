c7d6c5d4b33011d81d3861845314dad36cc67400
Lin Emsber <linemsber@gmail.com>
2016-12-10 14:20:26 +0800

    assign cookie

---
+++ LeetCode/assign_cookie_02.c
@@ -0,0 +1,53 @@
+void swap(int v[], int i, int j)
+{
+        int tmp;
+        tmp = v[i];
+        v[i] = v[j];
+        v[j] = tmp;
+}
+
+void quick_sort( int v[], int left, int right)
+{
+
+        int i, last;
+
+        if (left >= right)
+                return;
+
+        swap(v, left, (left + right) / 2);
+        last = left;
+
+        for (i=left+1; i<=right; i++){
+
+                if (v[i]<v[left]){
+                    last = last + 1;
+                    swap(v, last, i);
+                }
+        }
+
+        swap(v, left, last);
+        quick_sort(v, left, last-1);
+        quick_sort(v, last+1, right);
+
+}
+
+int findContentChildren(int* g, int gSize, int* s, int sSize)
+{
+        // iteration
+        int i = 0, j = 0;
+
+        // sort the both array
+        quick_sort(g, 0, gSize - 1);
+        quick_sort(s, 0, sSize - 1);
+
+        // while loop method
+        // compare the integer of both array
+        while (  i < gSize && j < sSize ){
+                if (g[i] <= s[j]){
+        		i++;
+        	}
+        	j++;
+        }
+
+        return i;
+}
