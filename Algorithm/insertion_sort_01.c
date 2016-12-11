62c86bbdbcd848cfd0750e713f4ce5e4459c894c
Lin Emsber <linemsber@gmail.com>
2016-12-11 11:46:22 +0800

    make algorithm as a function

--- Algorithm/insertion_sort_01.c
+++ Algorithm/insertion_sort_01.c
@@ -2,38 +2,49 @@
 
 #include <stdio.h>
 
-int main()
+int * insertion_sort(int *arr, int len)
 {
+	int i, j;
+	int tmp;
 
-	int i, j, num, temp, arr[20];
-
-	printf("Enter total elements: ");
-	scanf("%d", &num);
-
-	printf("Enter %d elements: ", num);
-	for (i = 0; i < num; i++) {
-		scanf("%d", &arr[i]);
-	}
-
-
-	// insertion sort
-	for ( i=1; i< num; i++){
-		temp = arr[i];
+	for ( i = 1; i < len; i++ ){
+		tmp = arr[i];
 		j = i-1;
 
-		while ( (j>=0) && (temp < arr[j]) ){
+		while ( (j>=0) && (tmp < arr[j]) ){
 			arr[j+1] = arr[j];
 			j = j - 1;
 		}
 
-		arr[j+1] = temp;
+		arr[j+1] = tmp;
 	}
 
-	// print out the results
-	printf("After Sorting: ");
-	for (i = 0; i < num; i++)
-		printf("%d\t", arr[i]);
+	return arr;
+}
 
+int main()
+{
+	int i;
+	int a[] = {10, 2, 4, 5, 22, 898, 84, 12, 103, 331, 56};
+	int length_a = sizeof(a) / sizeof(int);
+
+	// before sorting
+	printf("original a[]: ");
+	for (i=0; i<length_a; i++){
+		printf("%d  ", a[i]);
+	}
 	printf("\n");
 
+
+	// insertion sort
+	insertion_sort(a, length_a);
+
+	// after sorting
+	printf("original a[]: ");
+	for (i=0; i<length_a; i++){
+		printf("%d  ", a[i]);
+	}
+	printf("\n");
+
+	return 0;
 }
