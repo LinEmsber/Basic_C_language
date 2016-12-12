fef967c3d52bcbea0552b46d415e5f53acc7f1ec
Lin Emsber <linemsber@gmail.com>
2016-12-12 21:51:13 +0800

    add comment

--- Algorithm/quick_sort_01.c
+++ Algorithm/quick_sort_01.c
@@ -13,29 +13,31 @@ void swap(int v[], int i, int j)
 
 void quick_sort( int v[], int left, int right)
 {
-
 	int i, last;
 
+	// if the index of left element is equal or larger than the index of right element.
 	if (left >= right)
 		return;
 
-	swap(v, left, (left+right)/2);
+	// swap the most left element with the middle element
+	swap(v, left, (left + right) / 2);
+	// assign the last index as the most left index (the middle index)
 	last = left;
 
-	for (i=left+1; i<=right; i++){
+	for (i = left + 1; i <= right; i++){
 
-		// if (v[i]<v[left]){
-		//     last = last + 1;
-		//     swap(v, last, i);
-		// }
-
-		if (v[i]<v[left]){
+		if (v[i] < v[left]){
+			// last = last + 1;
+			// swap(v, last, i);
 			swap(v, ++last, i);
 		}
 	}
 
 	swap(v, left, last);
+
+	// quick sort the left side array
 	quick_sort(v, left, last-1);
+	// quick sort the right side array
 	quick_sort(v, last+1, right);
 
 }
