7027013cc0393b0310cad28bce7eb754f28be272
Lin Emsber <linemsber@gmail.com>
2016-12-12 21:39:54 +0800

    fix

--- LeetCode/assign_cookie_01.c
+++ LeetCode/assign_cookie_01.c
@@ -10,7 +10,6 @@ void swap(int v[], int i, int j)
 
 void quick_sort( int v[], int left, int right)
 {
-
         int i, last;
 
         if (left >= right)
@@ -19,7 +18,7 @@ void quick_sort( int v[], int left, int right)
         swap(v, left, (left + right) / 2);
         last = left;
 
-        for (i=left+1; i<=right; i++){
+        for (i = left + 1; i <= right; i++){
 
                 if (v[i]<v[left]){
                     last = last + 1;
