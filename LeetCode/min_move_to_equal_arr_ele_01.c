5c6a450d770dfd396a44aae07de25d2a5fb014f5
Lin Emsber <linemsber@gmail.com>
2016-12-18 17:42:12 +0800

    add explanation

--- LeetCode/min_move_to_equal_arr_ele_01.c
+++ LeetCode/min_move_to_equal_arr_ele_01.c
@@ -2,6 +2,10 @@
 
 #include <stdio.h>
 
+// Adding 1 to (n-1) elements is equivalent to subtracting 1 from one of the elements and adding 1 to all elements.
+// We must find the min number of (subtract 1 from any element) operations.
+// The only way to make all elements equal this way is to make them all equal to the min element of the array.
+// Hence, number of moves = sum(array) - n * min_element.
 
 int minMoves(int* num_arr, int numsSize)
 {
