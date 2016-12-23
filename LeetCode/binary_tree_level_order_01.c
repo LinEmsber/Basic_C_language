302bbd05adacd7a2bef08f9078492a2ebdc8f3c3
Lin Emsber <linemsber@gmail.com>
2016-12-23 08:11:50 +0800

    add comment

--- LeetCode/binary_tree_level_order_01.c
+++ LeetCode/binary_tree_level_order_01.c
@@ -1,3 +1,15 @@
+// Definition for a binary tree node.
+struct TreeNode {
+        int val;
+        struct TreeNode *left;
+        struct TreeNode *right;
+};
+
+/* 
+ *
+ *
+ *
+ */
 void traverse(struct TreeNode *root, int depth, int ***arr, int **columnSizes, int *returnSize)
 {
         if (!root)
@@ -12,18 +24,27 @@ void traverse(struct TreeNode *root, int depth, int ***arr, int **columnSizes, i
                 *arr = realloc(*arr, (depth + 1) * sizeof(int *));
                 (*arr)[depth] = NULL;
 
-                *columnSizes = realloc(*columnSizes, (depth + 1) * sizeof(int));
+                *columnSizes = realloc( *columnSizes, (depth + 1) * sizeof(int) );
                 (*columnSizes)[depth] = 0;
         }
 
-        (*arr)[depth] = realloc((*arr)[depth], ((*columnSizes)[depth] + 1) * sizeof(int));
-        (*arr)[depth][(*columnSizes)[depth]] = root->val;
+        (*arr)[depth] = realloc( (*arr)[depth], ((*columnSizes)[depth] + 1) * sizeof(int) );
+        (*arr)[depth][ (*columnSizes)[depth] ] = root->val;
+
+        // move to the next
         ++(*columnSizes)[depth];
 
         traverse(root->left, depth + 1, arr, columnSizes, returnSize);
         traverse(root->right, depth + 1, arr, columnSizes, returnSize);
 }
 
+/**
+ * @root: the root of tree
+ * @columnSizes: The sizes of the arrays are returned as *columnSizes array.
+ * @returnSize: An array of arrays of as returnSize.
+ *
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
 int **levelOrder(struct TreeNode *root, int **columnSizes, int *returnSize)
 {
         // pointer to pointer array for saving the result.
