b0b080a7bdf485ddfdad79e4ecd9f3ed4aa7f637
Lin Emsber <linemsber@gmail.com>
2016-12-28 23:55:36 +0800

    add tree

---
+++ Textbooks/Data_Strucutre_in_C/ch_5/tree.h
@@ -0,0 +1,26 @@
+/* tree.h */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// ======== typedef ========
+typedef struct node node_t;
+typedef struct tree tree_t;
+
+
+// ======== structure ========
+struct tree
+{
+        struct node *root;
+        int count;
+};
+
+struct node
+{
+        int value;
+        void *data;
+        struct node *left;
+        struct node *right;
+};
