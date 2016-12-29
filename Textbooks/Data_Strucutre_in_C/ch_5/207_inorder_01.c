cdc77133f923f28641f569a864faf61b3abad90e
Lin Emsber <linemsber@gmail.com>
2016-12-29 23:58:29 +0800

    fix inorder

--- Textbooks/Data_Strucutre_in_C/ch_5/207_inorder_01.c
+++ Textbooks/Data_Strucutre_in_C/ch_5/207_inorder_01.c
@@ -1,6 +1,6 @@
 /* inorder traversal */
 
-// gcc 207_inorder_01.c tree.c 
+// gcc 207_inorder_01.c tree.c
 
 #include <stdio.h>
 #include "tree.h"
@@ -10,7 +10,7 @@ void node_inorder(node_t *root)
 
         if (root){
                 node_inorder(root -> left);
-                printf("%d ", root -> value);
+                printf("%c ", root -> value);
                 node_inorder(root -> right);
         }
 }
@@ -20,15 +20,15 @@ int main()
         tree_t *t = tree_create();
         tree_init(t);
 
-        tree_insert_node(t, 1);
-        tree_insert_node(t, 5);
-        tree_insert_node(t, 6);
-        tree_insert_node(t, 22);
-        tree_insert_node(t, 99);
-        tree_insert_node(t, 10);
-        tree_insert_node(t, 3);
-        tree_insert_node(t, 1);
-        tree_insert_node(t, 1);
+        tree_insert_node(t, 'A');
+        tree_insert_node(t, 'B');
+        tree_insert_node(t, 'D');
+        tree_insert_node(t, '*');
+        tree_insert_node(t, '*');
+        tree_insert_node(t, '/');
+        tree_insert_node(t, '+');
+        tree_insert_node(t, 'C');
+        tree_insert_node(t, 'E');
 
         node_inorder(t -> root);
 
