93b5ca091d4990565c4be6bad6fcbb7c99ae7bdc
Lin Emsber <linemsber@gmail.com>
2017-01-14 11:02:12 +0800

    fix bug and add comment

--- Algorithm/print_list_02.c
+++ Algorithm/print_list_02.c
@@ -19,6 +19,7 @@ int * rn_list_gen( int len , int start, int end)
         int range = start - end;
 
         // memroy allocation
+        // In order to determine the end of list, add one more byte and set it as INT_MAX.
         ret_arr = (int *) malloc( (len + 1) * sizeof(int) );
         memset(ret_arr, 0, len + 1);
 
@@ -29,13 +30,17 @@ int * rn_list_gen( int len , int start, int end)
         for ( i = 0; i < len; i++ ){
                 *(ret_arr + i) = (rand() % range) + 1;
         }
-        *(ret_arr + i) = INT_MAX;
+        // set the last byte as INT_MAX
+        *(ret_arr + len) = INT_MAX;
 
         return ret_arr;
 }
 
-
-void print_list_INT_MAX(int * list, int len)
+/* print the list
+ *
+ * @list: the target list
+ */
+void print_list_INT_MAX(int * list)
 {
         int * ptr = list;
 
@@ -50,7 +55,7 @@ int main( int argc, char *argv[] )
 {
         int * arr = rn_list_gen( atoi(argv[1]), 10, 100 );
 
-        print_list_INT_MAX( arr, atoi(argv[1]) );
+        print_list_INT_MAX( arr );
 
         return 0;
 }
