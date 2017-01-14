6d74984bae638d274feb370a8207ba9b35abd22f
Lin Emsber <linemsber@gmail.com>
2017-01-14 12:45:58 +0800

    division in C

---
+++ Basic_C/other/division_modulus_in_c_01.c
@@ -0,0 +1,16 @@
+/* division and modulus operation in c */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("103 %% 10 = %d\n", 103 % 10);
+	printf("-103 %% 10 = %d\n", -103 % 10);
+	printf("103 %% -10 = %d\n", 103 % -10);
+	printf("-103 %% -10 = %d\n", -103 % -10);
+	
+	printf("103 / 10 = %d\n", 103 / 10);
+	printf("-103 / 10 = %d\n", -103 / 10);
+	printf("103 / -10 = %d\n", 103 / -10);
+	printf("-103 / -10 = %d\n", -103 / -10);
+}
