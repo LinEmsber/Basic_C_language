4e4c25f0b58109a478a3332177f8f807970faf9b
Lin Emsber <linemsber@gmail.com>
2017-01-15 15:03:10 +0800

    rename variable, list > arr

--- Algorithm/rand_num_arr_gen_01.c
+++ Algorithm/rand_num_arr_gen_01.c
@@ -1,17 +1,17 @@
-/* Using rand() to generate a random integer number list */
+/* Using rand() to generate a random integer number arr */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-/* generate a random number list in a specific range
+/* generate a random number arr in a specific range
  *
- * @len: the length of list
- * @start: the start number of list
- * @end: the end number of list
+ * @len: the length of arr
+ * @start: the start number of arr
+ * @end: the end number of arr
  */
-int * rn_list_gen( int len , int start, int end)
+int * rn_arr_gen( int len , int start, int end)
 {
         int i;
         int * ret_arr = NULL;
@@ -24,7 +24,7 @@ int * rn_list_gen( int len , int start, int end)
         // rand seed use time
         srand( (unsigned) time(NULL) );
 
-        // generate list
+        // generate arr
         for ( i = 0; i < len; i++ ){
                 *(ret_arr + i) = (rand() % range) + 1;
         }
@@ -32,24 +32,24 @@ int * rn_list_gen( int len , int start, int end)
         return ret_arr;
 }
 
-/* print the list with specific lenght
+/* print the arr with specific lenght
  *
- * @list: the target list
- * @len: the length of list
+ * @arr: the target arr
+ * @len: the length of arr
  */
-void print_list(int *list, int len)
+void print_list(int *arr, int len)
 {
         int i;
 
         for ( i = 0; i < len; i++){
-                printf("%d ", *(list + i) );
+                printf("%d ", *(arr + i) );
         }
         printf("\n");
 }
 
 int main( int argc, char *argv[] )
 {
-        int * arr = rn_list_gen( atoi(argv[1]), 10, 100 );
+        int * arr = rn_arr_gen( atoi(argv[1]), 10, 100 );
 
         print_list( arr, atoi(argv[1]) );
 
