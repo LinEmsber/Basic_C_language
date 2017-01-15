c15ad2d2557b42c5045dfa84f019d0868abde1ba
Lin Emsber <linemsber@gmail.com>
2017-01-15 15:07:00 +0800

    fix typo

--- Algorithm/rand_num_arr_gen_01.c
+++ Algorithm/rand_num_arr_gen_01.c
@@ -1,15 +1,15 @@
-/* Using rand() to generate a random integer number arr */
+/* Using rand() to generate a random integer number array */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-/* generate a random number arr in a specific range
+/* generate a random number array in a specific range
  *
- * @len: the length of arr
- * @start: the start number of arr
- * @end: the end number of arr
+ * @len: the length of array
+ * @start: the start number of array
+ * @end: the end number of array
  */
 int * rn_arr_gen( int len , int start, int end)
 {
@@ -32,12 +32,12 @@ int * rn_arr_gen( int len , int start, int end)
         return ret_arr;
 }
 
-/* print the arr with specific lenght
+/* print the array with specific lenght
  *
- * @arr: the target arr
- * @len: the length of arr
+ * @arr: the target array
+ * @len: the length of array
  */
-void print_list(int *arr, int len)
+void print_arr(int *arr, int len)
 {
         int i;
 
@@ -51,7 +51,7 @@ int main( int argc, char *argv[] )
 {
         int * arr = rn_arr_gen( atoi(argv[1]), 10, 100 );
 
-        print_list( arr, atoi(argv[1]) );
+        print_arr( arr, atoi(argv[1]) );
 
         return 0;
 }
