bf67133b5357c9ae2d2ff0f1457b8cfcaed83834
Lin Emsber <linemsber@gmail.com>
2017-01-23 22:47:01 +0800

    move files to new repo

--- Textbooks/Understanding_and_Using_C_Pointers/118_concate_str_01.c
+++
@@ -1,9 +0,0 @@
-/* Concatenating strings */
-
-char *error = "ERROR: ";
-char *error_msg = "Not enough memory";
-
-char *buffer = (char *) malloc (strlen(error) + strlne(error_msg) + 1 );
-
-strcpy(buffer, error);
-strcat(buffer, error_msg);
--- Textbooks/Understanding_and_Using_C_Pointers/README.md
+++
@@ -1,4 +0,0 @@
-# The implementation of Understanding and Using C pointers
-
-20160507:
-25_double_pointer_01.c
