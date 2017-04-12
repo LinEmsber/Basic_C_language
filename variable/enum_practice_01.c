af524a32f18051abce8cabe38caadbc32aa94bbc
Lin Emsber <linemsber@gmail.com>
2017-04-12 22:39:33 +0800

    Add more and clear example of enum.
    1. Move content of enum_practice_02.c to enum_practice_01.c
    2. Add more printf() to show the result.

--- variable/enum_practice_01.c
+++ variable/enum_practice_01.c
@@ -2,18 +2,43 @@
 
 #include <stdio.h>
 
-enum week{ sunday, monday, tuesday, wednesday, thursday, friday, saturday};
+/* week */
+enum week{
+	sunday, monday, tuesday, wednesday, thursday, friday, saturday
+};
+
+/* no = 0, yes = 1 */
+enum boolean {
+	NO, YES
+};
+
+/* Month */
+enum month {
+	Jan = 1, Feb, Mar,
+	Apr, May, Jun,
+	Jul, Aug, Sep,
+	Oct, Nov, Dec
+};
 
 int main()
 {
 
 	enum week today;
-	today=wednesday;
-	/*
-	today = asdf;
-	the error situation, only accept variables have already in the week function
-	*/
-	printf("%d day\n",today+1);
+	enum boolean boolean_entry;
+	enum month month_entry;
+
+	today = wednesday;
+
+	printf("sizeof(enum week): %zu\n", sizeof(enum week));
+	printf("today: %d\n", today);
+	printf("today + 1: %d\n", today + 1);
+	printf("today + 2: %d\n", today + 2);
+
+	printf("boolean_entry: %d\n", boolean_entry);
+	printf("boolean_entry + 1: %d\n", boolean_entry + 1);
+
+	printf("month_entry: %d\n", month_entry);
+	printf("month_entry + 5: %d\n", month_entry + 5);
 
 	return 0;
 }
